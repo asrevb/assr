1493cc5f9dbbe05663792ef9bd231e88df6d7a94
asrevb <117964846+asrevb@users.noreply.github.com>
2023-02-13 02:53:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,21 +11,21 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 SESSION_NAME = getenv("SESSION_NAME", "")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9") # @ هنا ضع يوزر حسابك بدون 
-ALIVE_NAME = getenv("ALIVE_NAME", "sonng") # هنا ضع اسم حسابك
-BOT_USERNAME = getenv("BOT_USERNAME", "") # @ هنا ضع يوزر البوت بدون 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/X02lx/RrRRR") 
+OWNER_USERNAME     =     getenv ( "OWNER_USERNAME"    ،    "aasr_6" ) # @ هنا ضع يوزر حسابك بدو
+ALIVE_NAME    =    getenv ( "ALIVE_NAME"   ،   "مِـيِـۆڒݢ〠🎸🎧" ) # هنا ضع اسم حسابك
+BOT_USERNAME  =  getenv ( "BOT_USERNAME" ، "asr_77bot" ) # @ هنا ضع يوزر البوت بدون
+UPSTREAM_REPO  =  getenv ( "UPSTREAM_REPO" ، "https://github.com/X02lx/RrRRR" )
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main") #لا تغير هاذة القيمة
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60")) #لا تغير هاذة القيمة
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9") # @ هنا ضغ يوزر كروبك بدون 
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444") # @ هنا ضغ يوزر قناتك بدون
+GROUP_SUPPORT   =   getenv ( "GROUP_SUPPORT"  ،  "xvxzvxx" ) # @ هنا ضغ يوزر كروبك بدون
+UPDATES_CHANNEL   =   getenv ( "UPDATES_CHANNEL"  ،  "asniim" ) # @ هنا ضغ يوزر قناتك بدون
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
+OWNER_ID   =   list ( الخريطة ( int  ،  getenv ( "OWNER_ID"  ،  "5361809514" ). split ()))
                                              #هنا ضع ايدي المطور فوق و الاعلئ
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+SUDO_USERS   =   قائمة ( خريطة ( int  ،  getenv ( "SUDO_USERS"  ،  "5361809514" ). split ()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
